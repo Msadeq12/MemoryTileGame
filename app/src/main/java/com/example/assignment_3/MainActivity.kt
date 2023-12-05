9eb3038963f6041aeae71d4bcaebdb7809ba37e5
Mohammad Sadeq Khandakar <msadeq27@yahoo.com>
2023-12-05 16:13:05 -0500

    Added Navigation Drawer to navigate between fragments.

--- app/src/main/java/com/example/assignment_3/MainActivity.kt
+++ app/src/main/java/com/example/assignment_3/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.example.assignment_3
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.Window
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, WelcomeFragment()).commit()
+            navigationView.setCheckedItem(R.id.nav_welcome)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_welcome -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, WelcomeFragment()).commit()
+            R.id.nav_score -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, ScoreFragment()).commit()
+            R.id.nav_game -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, GameFragment()).commit()
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            onBackPressedDispatcher.onBackPressed()
+        }
     }
 }
